a78862d6608aebffa92c0637660cd93f6e86e96e
MatthisDom <95086767+MatthisDom@users.noreply.github.com>
2025-02-13 15:17:13 +0100

    mise à jour commande (flèche directionelles)

--- js/ecouteurs.js
+++ js/ecouteurs.js
@@ -1,32 +1,40 @@
 function initListeners(inputStates, canvas) {
     window.onkeydown = (event) => {
         console.log("Touche pressée : " + event.key);
-        if(event.key === "d") {
+        if(event.key === "d"|| event.key === "ArrowRight")
+        {
             inputStates.ArrowRight = true;
         }
-        if(event.key === "q") {
+        if(event.key === "q" || event.key === "ArrowLeft")
+        {
             inputStates.ArrowLeft = true;
         }
-        if(event.key === "z") {
+        if(event.key === "z" || event.key === "ArrowUp")
+        {
             inputStates.ArrowUp = true;
         }
-        if(event.key === "s") {
+        if(event.key === "s" || event.key === "ArrowDown")
+        {
             inputStates.ArrowDown = true;
         }
     }
 
     window.onkeyup = (event) => {
         console.log("Touche relachée : " + event.key);
-        if(event.key === "d") {
+        if(event.key === "d" || event.key === "ArrowRight") 
+        {
             inputStates.ArrowRight = false;
         }
-        if(event.key === "q") {
+        if(event.key === "q" || event.key === "ArrowLeft") 
+        {
             inputStates.ArrowLeft = false;
         }
-        if(event.key === "z") {
+        if(event.key === "z" || event.key === "ArrowUp")
+        {
             inputStates.ArrowUp = false;
         }
-        if(event.key === "s") {
+        if(event.key === "s" || event.key === "ArrowDown")
+        {
             inputStates.ArrowDown = false;
         }
     }
